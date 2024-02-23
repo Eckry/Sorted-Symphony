127aaa11e2787e26e021a6df758cd33dc476c0d9
Eckry <112786854+Eckry@users.noreply.github.com>
2024-02-22 21:18:35 -0600

    add selection menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import { Selection } from "./components/Selection";
 
 function App() {
   return (
     <main className="main-container">
       <Header />
+      <Selection />
     </main>
   );
 }
