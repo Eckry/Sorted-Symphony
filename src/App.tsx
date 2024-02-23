fe2daa7106475a571ced126be709729a83523f11
Eckry <112786854+Eckry@users.noreply.github.com>
2024-02-22 21:51:41 -0600

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header";
 import { Selection } from "./components/Selection";
-import { type Algorithm } from "./types";
-import { algorithms } from "./consts";
 
 function App() {
-  const [selected, setSelected] = useState<Algorithm>(algorithms.BUBBLESORT);
-
-  const handleOnSetSelected = (name: Algorithm) => {
-    setSelected(name);
-  }
-
   return (
     <main className="main-container">
       <Header />
-      <Selection selected={selected} handleOnSetSelected={handleOnSetSelected}/>
+      <Selection />
     </main>
   );
 }
