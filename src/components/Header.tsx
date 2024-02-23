f7a56098096cad41066e90ce23affcc5fd1faaa7
Eckry <112786854+Eckry@users.noreply.github.com>
2024-02-22 21:01:01 -0600

    create header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import "./styles/Header.css"
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header-title">Sorted Symphony</h1>
+      <p className="header-phrase">Hear whatever sort algorithm you want.</p>
+    </header>
+  );
+};
