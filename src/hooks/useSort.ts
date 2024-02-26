444090c4d26ef5430d5929b30853db82827c2318
Eckry <112786854+Eckry@users.noreply.github.com>
2024-02-26 17:52:33 -0600

    move Funcions to helpers.ts

--- src/hooks/useSort.ts
+++ src/hooks/useSort.ts
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { algorithms } from "../consts";
 import { useSelected } from "./useSelected";
-import { Block } from "../types";
+import { resetColor, swap } from "../helpers";
 
 const initialBlocks = [
   { val: 22, color: "white" },
@@ -30,40 +30,11 @@ const initialBlocks = [
   { val: 1, color: "white" },
 ];
 
-const sleep = (delay: number) =>
-  new Promise((resolve) => setTimeout(resolve, delay));
-
 export const useSort = () => {
   const { selected } = useSelected();
   const [blocks, setBlocks] = useState(initialBlocks);
 
   useEffect(() => {
-    async function swap(i: number, j: number, prevBlocks: Block[]) {
-      const newBlocks = prevBlocks.map((block, idx) => {
-        if (idx === i) return { val: prevBlocks[j].val, color: "white" };
-        if (idx === j) return { val: prevBlocks[i].val, color: "red" };
-        return { ...block, color: "white" };
-      });
-      await sleep(100);
-      return newBlocks;
-    }
-
-    async function resetColor(prevBlocks: Block[]) {
-      for (let i = 0; i < prevBlocks.length; i++) {
-        const newBlocks = prevBlocks.map((block, idx) => {
-          if (idx === i) return { ...block, color: "goldenrod" };
-          return { ...block, color: "white" };
-        });
-        prevBlocks = [...newBlocks];
-        setBlocks(newBlocks);
-        await sleep(50);
-      }
-      const newBlocks = prevBlocks.map((block) => {
-        return { ...block, color: "white" };
-      });
-      setBlocks(newBlocks);
-    }
-
     async function bubbleSort() {
       let prevBlocks = structuredClone(blocks);
       for (let i = 0; i < blocks.length; i++) {
@@ -76,8 +47,9 @@ export const useSort = () => {
           }
         }
       }
-      await resetColor(prevBlocks);
+      await resetColor(prevBlocks, setBlocks);
     }
+    
     switch (selected) {
       case algorithms.BUBBLESORT:
         bubbleSort();
@@ -85,5 +57,5 @@ export const useSort = () => {
     }
   }, [selected]);
 
-  return { blocks };
+  return { blocks, setBlocks };
 };
