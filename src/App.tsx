eecbed6d61292c05f347b49b2a0f5233a295aebb
Eckry <112786854+Eckry@users.noreply.github.com>
2024-02-26 17:52:06 -0600

    add SortOptions

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import "./App.css";
 import { Blocks } from "./components/Blocks";
 import { Header } from "./components/Header";
 import { Selection } from "./components/Selection";
+import { SortOptions } from "./components/SortOptions";
+import { useSort } from "./hooks/useSort";
 
 function App() {
+  const { blocks, setBlocks } = useSort();
   return (
     <main className="main-container">
       <Header />
       <Selection />
-      <Blocks />
+      <Blocks blocks={blocks}/>
+      <SortOptions blocks={blocks} setBlocks={setBlocks}/>
     </main>
   );
 }
