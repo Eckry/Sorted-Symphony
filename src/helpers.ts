2fb072aefc06f2bfd12e8c882e12917e7704fbc2
Eckry <112786854+Eckry@users.noreply.github.com>
2024-02-26 18:44:00 -0600

    add lowShuffle

--- src/helpers.ts
+++ src/helpers.ts
@@ -6,7 +6,20 @@ export const shuffle = (array: Block[]) => {
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
-}
+};
+
+export const lowShuffle = (array: Block[]) => {
+  array.sort((a, b) => a.val - b.val);
+
+  const increment = Math.floor(array.length / 3);
+
+  for (let i = 0; i < array.length; i += increment) {
+    const j = Math.floor(Math.random() * array.length);
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array
+};
 
 export const sleep = (delay: number) =>
   new Promise((resolve) => setTimeout(resolve, delay));
@@ -19,9 +32,12 @@ export const swap = async (i: number, j: number, prevBlocks: Block[]) => {
   });
   await sleep(10);
   return newBlocks;
-}
+};
 
-export const resetColor = async (prevBlocks: Block[], setBlocks: (blocks: Block[]) => void) => {
+export const resetColor = async (
+  prevBlocks: Block[],
+  setBlocks: (blocks: Block[]) => void
+) => {
   for (let i = 0; i < prevBlocks.length; i++) {
     const newBlocks = prevBlocks.map((block, idx) => {
       if (idx === i) return { ...block, color: "goldenrod" };
@@ -35,4 +51,4 @@ export const resetColor = async (prevBlocks: Block[], setBlocks: (blocks: Block[
     return { ...block, color: "white" };
   });
   setBlocks(newBlocks);
-}
+};
