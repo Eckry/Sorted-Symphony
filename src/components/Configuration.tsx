b7e070fdd55731daa82c5b60a66662e6330affa6
Eckry <112786854+Eckry@users.noreply.github.com>
2024-02-27 21:48:55 -0600

    add input range and velocity state handler

--- src/components/Configuration.tsx
+++ src/components/Configuration.tsx
@@ -1,15 +1,43 @@
+import { ConfigurationVelocity } from "../types";
+import "./styles/Configuration.css";
+
 interface Props {
   changeIsSorting: () => void;
+  changeVelocity: ({ velocity }: ConfigurationVelocity) => void;
 }
 
-export const Configuration: React.FC<Props> = ({ changeIsSorting }) => {
+export const Configuration: React.FC<Props> = ({
+  changeIsSorting,
+  changeVelocity,
+}) => {
   const handleSort = () => {
     changeIsSorting();
   };
 
+  const handleChangeVelocity = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newVelocity = Number(event.target.value);
+    changeVelocity({ velocity: newVelocity });
+  };
+
   return (
-    <section>
-      <button onClick={handleSort}>SORT</button>
+    <section className="configuration-container">
+      <div className="configuration-range">
+        <input id="configuration-quantity" type="range" />
+        <label htmlFor="configuration-quantity">Elements</label>
+      </div>
+      <button className="play-sort" onClick={handleSort}>
+        II
+      </button>
+      <div className="configuration-range">
+        <input
+          onChange={handleChangeVelocity}
+          id="configuration-velocity"
+          type="range"
+          min={1}
+          max={100}
+        />
+        <label htmlFor="configuration-velocity">Velocity</label>
+      </div>
     </section>
   );
 };
