7280cbfc06d1a0336ae73415832b9f3531ccab76
Eckry <112786854+Eckry@users.noreply.github.com>
2024-02-27 18:58:19 -0600

    add configuration component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import "./App.css";
 import { Blocks } from "./components/Blocks";
+import { Configuration } from "./components/Configuration";
 import { Header } from "./components/Header";
 import { Selection } from "./components/Selection";
 import { SortOptions } from "./components/SortOptions";
 import { useSort } from "./hooks/useSort";
 
 function App() {
-  const { blocks, setBlocks } = useSort();
+  const { blocks, setBlocks, changeIsSorting } = useSort();
   return (
     <main className="main-container">
       <Header />
       <Selection />
-      <Blocks blocks={blocks}/>
-      <SortOptions blocks={blocks} setBlocks={setBlocks}/>
+      <Blocks blocks={blocks} />
+      <SortOptions blocks={blocks} setBlocks={setBlocks} />
+      <Configuration changeIsSorting={changeIsSorting} />
     </main>
   );
 }
