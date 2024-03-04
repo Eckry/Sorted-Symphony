a3724973760c5eaeb66cd258d7dbd7f91bd439db
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-03 21:46:18 -0600

    add Language type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,8 @@
-import { algorithms } from "./consts";
+import { algorithms, languages } from "./consts";
 
 export type Algorithm = typeof algorithms[keyof typeof algorithms]
+
+export type Language = typeof languages[keyof typeof languages]
 
 export type Block = {
   val: number
