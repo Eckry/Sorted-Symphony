875e1559b958f26f005b43be7c55ab23c02df4dd
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-03 21:47:22 -0600

    add code for language selected

--- src/hooks/useSelected.ts
+++ src/hooks/useSelected.ts
@@ -1,7 +1,11 @@
-import { useContext } from "react"
+import { useContext } from "react";
 import { SelectedContext } from "../context/Selected";
+import { codes } from "../codes";
 
 export const useSelected = () => {
-  const {selected, setSelected} = useContext(SelectedContext);
-  return {selected, setSelected}
-}
+  const { selected, setSelected } = useContext(SelectedContext);
+
+  const code = codes[selected]
+
+  return { selected, code, setSelected };
+};
