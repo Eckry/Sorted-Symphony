b92c993977f409d3e8b2b46141fcaa0be4ae295d
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-04 16:41:28 -0600

    add python and cpp code for SelectionSort

--- src/codes.ts
+++ src/codes.ts
@@ -3,20 +3,16 @@ export const codes = {
     javascript: `for(let i = 0; i < n; i++){
   for(let j = 0; j + 1 < n - i; j++){
     if(arr[j] > arr[j + 1]){
-      let temp = arr[j];
-      arr[j] = arr[j + 1];
-      arr[j + 1] = temp;
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
     }
   }
 }`,
     python: `for i in range(n):
   for j in range(n - i - 1):
     if arr[j] > arr[j + 1]:
-      temp = arr[j]
-      arr[j] = arr[j + 1]
-      arr[j + 1] = temp`,
-      
-    "cpp": `for(int i = 0; i < n; i++){
+      arr[j], arr[j + 1] = arr[j + 1], arr[j]`,
+
+    cpp: `for(int i = 0; i < n; i++){
   for(int j = 0; j + 1 < n - i; j++){
     if(arr[j] > arr[j + 1]){
       int temp = arr[j];
@@ -28,13 +24,40 @@ export const codes = {
   },
   SelectionSort: {
     javascript: `for(let i = 0; i < n; i++){
-  let min = i
+
+  let minIdx = i;
+
   for(let j = i + 1; j < n; j++){
-    if(arr[min] > arr[j]){
-      min = j
+    if(arr[minIdx] > arr[j]){
+      minIdx = j;
     }
   }
-  arr[i] = arr[min]
+  
+  [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+}`,
+    python: `for i in range(n):
+
+  minIdx = i
+
+  for j in range(i + 1, n):
+    if arr[j] < arr[minIdx]:
+      minIdx = j
+
+  arr[i], arr[minIdx] = arr[minIdx], arr[i]`,
+
+    cpp: `for(int i = 0; i < n; i++){
+
+  int minIdx = i;
+
+  for(int j = i + 1; j < n; j++){
+    if(arr[j] < arr[minIdx]){
+      minIdx = j;
+    }
+  }
+
+  int temp = arr[minIdx];
+  arr[minIdx] = arr[i];
+  arr[i] = temp;
 }`,
   },
   QuickSort: {},
