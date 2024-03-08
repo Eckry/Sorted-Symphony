abe86997c066af73f1417d6a4c4d4b1679ecb527
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-07 20:56:07 -0600

    add mergeSort

--- src/hooks/useSort.ts
+++ src/hooks/useSort.ts
@@ -2,7 +2,13 @@
 import { useEffect, useState, useRef } from "react";
 import { algorithms } from "../consts";
 import { useSelected } from "./useSelected";
-import { isSorted, resetColor, swap, swapAndPaintBoth } from "../helpers";
+import {
+  insert,
+  isSorted,
+  resetColor,
+  swap,
+  swapAndPaintBoth,
+} from "../helpers";
 import { Block, ConfigurationElements, ConfigurationVelocity } from "../types";
 
 const initialBlocks = [
@@ -70,7 +76,10 @@ export const useSort = () => {
       return;
     }
 
-    if (!isSortingRef.current) return;
+    function resetMerge(prevMerge: Block[]) {
+      setBlocks(prevMerge);
+    }
+
     async function bubbleSort() {
       let prevBlocks = structuredClone(blocks);
       for (let i = 0; i < blocks.length; i++) {
@@ -123,7 +132,7 @@ export const useSort = () => {
         const pivot = prevBlocks[center];
         let i = first;
         let j = last;
-        
+
         do {
           while (prevBlocks[i].val < pivot.val) i++;
           while (prevBlocks[j].val > pivot.val) j--;
@@ -145,8 +154,105 @@ export const useSort = () => {
         if (i < last) await executeQuickSort(i, last);
       }
 
-      if(!isSorted(prevBlocks)) await executeQuickSort(0, prevBlocks.length - 1);
+      if (!isSorted(prevBlocks))
+        await executeQuickSort(0, prevBlocks.length - 1);
       if (!isSortingRef.current) return stop(prevBlocks);
+      await resetColor(prevBlocks, setBlocks, configuration.velocity);
+      setIsSorting(false);
+      isSortingRef.current = false;
+    }
+
+    async function mergeSort() {
+      let prevBlocks = structuredClone(blocks);
+      let prevMerge = structuredClone(prevBlocks);
+      async function merge(p: number, q: number, r: number) {
+        const n1 = q - p + 1;
+        const n2 = r - q;
+        const L: number[] = Array(n1);
+        const M: number[] = Array(n2);
+
+        for (let i = 0; i < n1; i++) L[i] = prevBlocks[p + i].val;
+        for (let j = 0; j < n2; j++) M[j] = prevBlocks[q + 1 + j].val;
+
+        let i = 0;
+        let j = 0;
+        let k = p;
+
+        while (i < n1 && j < n2) {
+          if (!isSortingRef.current) return;
+          prevBlocks = await insert(
+            prevBlocks,
+            L,
+            k,
+            i,
+            configuration.velocity
+          );
+          if (L[i] <= M[j]) {
+            setBlocks(prevBlocks);
+            i++;
+          } else {
+            prevBlocks = await insert(
+              prevBlocks,
+              M,
+              k,
+              j,
+              configuration.velocity
+            );
+            setBlocks(prevBlocks);
+            j++;
+          }
+          k++;
+        }
+
+        while (i < n1) {
+          if (!isSortingRef.current) return;
+          prevBlocks = await insert(
+            prevBlocks,
+            L,
+            k,
+            i,
+            configuration.velocity
+          );
+          setBlocks(prevBlocks);
+          i++;
+          k++;
+        }
+
+        while (j < n2) {
+          if (!isSortingRef.current) return;
+          prevBlocks = await insert(
+            prevBlocks,
+            M,
+            k,
+            j,
+            configuration.velocity
+          );
+          setBlocks(prevBlocks);
+          j++;
+          k++;
+        }
+        if (!isSortingRef.current) return;
+        prevMerge = structuredClone(prevBlocks);
+      }
+
+      async function executeMergeSort(l: number, r: number) {
+        if (!isSortingRef.current) return;
+        if (l < r) {
+          const m = l + Math.floor((r - l) / 2);
+          await executeMergeSort(l, m);
+          await executeMergeSort(m + 1, r);
+          await merge(l, m, r);
+        }
+      }
+
+      if (!isSorted(prevBlocks))
+        await executeMergeSort(0, prevBlocks.length - 1);
+      
+      if (!isSortingRef.current) {
+        resetMerge(prevMerge);
+        return stop(prevMerge);
+      }
+
       await resetColor(prevBlocks, setBlocks, configuration.velocity);
       setIsSorting(false);
       isSortingRef.current = false;
@@ -161,6 +267,9 @@ export const useSort = () => {
         break;
       case algorithms.QUICKSORT:
         quickSort();
+        break;
+      case algorithms.MERGESORT:
+        mergeSort();
         break;
     }
   }, [isSorting]);
