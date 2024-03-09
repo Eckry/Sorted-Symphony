3b34da66072e9681759227fd2b0a97475560013f
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-09 16:28:08 -0600

    create Home page

---
+++ src/routes/Home.tsx
@@ -0,0 +1,41 @@
+import "./styles/Home.css"
+import { Blocks } from "../components/Blocks";
+import { Code } from "../components/Code";
+import { Configuration } from "../components/Configuration";
+import { Description } from "../components/Description";
+import { Header } from "../components/Header";
+import { Selection } from "../components/Selection";
+import { SortOptions } from "../components/SortOptions";
+import { useSort } from "../hooks/useSort";
+
+export const Home = () => {
+  const {
+    blocks,
+    setBlocks,
+    changeIsSorting,
+    isSorting,
+    changeVelocity,
+    changeElements,
+  } = useSort();
+  return (
+    <main className="main-container">
+      <Header />
+      <Selection />
+      <Blocks blocks={blocks} />
+      <SortOptions
+        isSorting={isSorting}
+        blocks={blocks}
+        setBlocks={setBlocks}
+      />
+      <Configuration
+        changeVelocity={changeVelocity}
+        changeIsSorting={changeIsSorting}
+        changeElements={changeElements}
+        blocks={blocks}
+        isSorting={isSorting}
+      />
+      <Code />
+      <Description />
+    </main>
+  );
+}
