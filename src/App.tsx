9e0403858e5b78e1004749d9e4aebe2d5a789fb6
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-09 16:28:03 -0600

    move Home page to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,16 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
-import { Blocks } from "./components/Blocks";
-import { Code } from "./components/Code";
-import { Configuration } from "./components/Configuration";
-import { Description } from "./components/Description";
-import { Header } from "./components/Header";
-import { Selection } from "./components/Selection";
-import { SortOptions } from "./components/SortOptions";
-import { useSort } from "./hooks/useSort";
+import { Home } from "./routes/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 function App() {
-  const {
-    blocks,
-    setBlocks,
-    changeIsSorting,
-    isSorting,
-    changeVelocity,
-    changeElements,
-  } = useSort();
-  return (
-    <main className="main-container">
-      <Header />
-      <Selection />
-      <Blocks blocks={blocks} />
-      <SortOptions
-        isSorting={isSorting}
-        blocks={blocks}
-        setBlocks={setBlocks}
-      />
-      <Configuration
-        changeVelocity={changeVelocity}
-        changeIsSorting={changeIsSorting}
-        changeElements={changeElements}
-        blocks={blocks}
-        isSorting={isSorting}
-      />
-      <Code />
-      <Description />
-    </main>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
