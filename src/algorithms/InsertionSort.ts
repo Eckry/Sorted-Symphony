fe42b2955739ccf0d31ae2475e45f1a36b3bcd2f
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-08 21:47:23 -0600

    create InsertionSort

---
+++ src/algorithms/InsertionSort.ts
@@ -0,0 +1,53 @@
+import { useRef } from "react";
+import { insert, resetColor, stop } from "../helpers";
+import { Block, Configuration } from "../types";
+
+export const InsertionSort = () => {
+  const isSortingRef = useRef(true);
+
+  const stopInsertionSort = () => {
+    isSortingRef.current = false;
+  };
+
+  const initInsertionSort = async (
+    blocks: Block[],
+    setBlocks: (newBLocks: Block[]) => void,
+    configuration: Configuration,
+    setIsSorting: (newIsSorting: boolean) => void
+  ) => {
+    isSortingRef.current = true;
+    let prevBlocks = structuredClone(blocks);
+    let prevStatus = structuredClone(blocks);
+    for (let i = 1; i < prevBlocks.length; i++) {
+      prevStatus = structuredClone(prevBlocks);
+      const key = prevBlocks[i].val;
+      let j = i - 1;
+      while (j >= 0 && key < prevBlocks[j].val) {
+        if (!isSortingRef.current) break;
+        prevBlocks = await insert(
+          prevBlocks,
+          prevBlocks[j].val,
+          j + 1,
+          configuration.velocity
+        );
+        setBlocks(prevBlocks);
+        j--;
+      }
+
+      if (!isSortingRef.current) break;
+      prevBlocks = await insert(prevBlocks, key, j + 1, configuration.velocity);
+      setBlocks(prevBlocks);
+    }
+
+    if (!isSortingRef.current) {
+      setBlocks(prevStatus);
+      return stop(prevStatus, setBlocks);
+    }
+
+    await resetColor(prevBlocks, setBlocks, configuration.velocity);
+    isSortingRef.current = false;
+    setIsSorting(false);
+  };
+
+  return { stopInsertionSort, initInsertionSort };
+};
