22ee15a0c42e6fb14a6f9559fc481634965110b9
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-08 21:47:32 -0600

    create QuickSort

---
+++ src/algorithms/QuickSort.ts
@@ -0,0 +1,55 @@
+import { useRef } from "react";
+import { isSorted, resetColor, stop, swapAndPaintBoth } from "../helpers";
+import { Block, Configuration } from "../types";
+
+export const QuickSort = () => {
+  const isSortingRef = useRef(true);
+
+  const stopQuickSort = () => {
+    isSortingRef.current = false;
+  };
+
+  const initQuickSort = async (
+    blocks: Block[],
+    setBlocks: (newBLocks: Block[]) => void,
+    configuration: Configuration,
+    setIsSorting: (newIsSorting: boolean) => void
+  ) => {
+    isSortingRef.current = true;
+    let prevBlocks = structuredClone(blocks);
+    async function executeQuickSort(first: number, last: number) {
+      const center = Math.floor((first + last) / 2);
+      const pivot = prevBlocks[center];
+      let i = first;
+      let j = last;
+
+      do {
+        while (prevBlocks[i].val < pivot.val) i++;
+        while (prevBlocks[j].val > pivot.val) j--;
+        if (!isSortingRef.current) return;
+        if (i <= j) {
+          prevBlocks = await swapAndPaintBoth(
+            i,
+            j,
+            prevBlocks,
+            configuration.velocity
+          );
+          setBlocks(prevBlocks);
+          i++;
+          j--;
+        }
+      } while (i <= j);
+
+      if (first < j) await executeQuickSort(first, j);
+      if (i < last) await executeQuickSort(i, last);
+    }
+
+    if (!isSorted(prevBlocks)) await executeQuickSort(0, prevBlocks.length - 1);
+    if (!isSortingRef.current) return stop(prevBlocks, setBlocks);
+    await resetColor(prevBlocks, setBlocks, configuration.velocity);
+    isSortingRef.current = false;
+    setIsSorting(false);
+  };
+
+  return { stopQuickSort, initQuickSort };
+};
