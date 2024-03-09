d94da755bf89c059f41c0b219d2bb6c09e6b25cc
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-09 16:13:46 -0600

    add java code for bubblesort

--- src/codes.ts
+++ src/codes.ts
@@ -21,6 +21,18 @@ export const codes = {
     }
   }
 }`,
+    java: `int n = arr.length;
+int temp = 0;
+
+for(int i = 0; i < n; i++){
+  for(int j = 0; j + 1 < n - i; j++){
+    if(arr[j] > arr[j + 1]){
+      temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+}`,
   },
   SelectionSort: {
     javascript: `for(let i = 0; i < n; i++){
