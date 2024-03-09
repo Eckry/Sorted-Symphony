5b9ae034f78dce05cd7c31359891a5f4c0c9f974
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-08 21:47:09 -0600

    create BubbleSort

---
+++ src/algorithms/BubbleSort.ts
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+import { resetColor, stop, swap } from "../helpers";
+import { Block, Configuration } from "../types";
+
+export const BubbleSort = () => {
+  const isSortingRef = useRef(true);
+
+  const stopBubbleSort = () => {
+    isSortingRef.current = false;
+  };
+
+  const initBubbleSort = async (
+    blocks: Block[],
+    setBlocks: (newBLocks: Block[]) => void,
+    configuration: Configuration,
+    setIsSorting: (newIsSorting: boolean) => void
+  ) => {
+    isSortingRef.current = true;
+    let prevBlocks = structuredClone(blocks);
+    for (let i = 0; i < blocks.length; i++) {
+      for (let j = 0; j + 1 < blocks.length - i; j++) {
+        const { val: a } = prevBlocks[j];
+        const { val: b } = prevBlocks[j + 1];
+        if (!isSortingRef.current) return stop(prevBlocks, setBlocks);
+        if (a > b) {
+          prevBlocks = await swap(j, j + 1, prevBlocks, configuration.velocity);
+          setBlocks(prevBlocks);
+        }
+      }
+    }
+    await resetColor(prevBlocks, setBlocks, configuration.velocity);
+    isSortingRef.current = false;
+    setIsSorting(false);
+  };
+
+  return { stopBubbleSort, initBubbleSort };
+};
