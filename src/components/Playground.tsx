a6e99b8605a360230bb481cfa6fb285b89f4670c
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-09 21:44:31 -0600

    add activateColumn button

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,27 +1,82 @@
 import "./styles/Playground.css";
-import { Algorithm } from "../types";
-import { sortOptions } from "../consts";
+import { Algorithm, SortOption, SortingAlgorithms } from "../types";
+import { algorithms, sortOptions } from "../consts";
 import { PlaygroundBlock } from "./PlaygroundBlock";
+import { useState } from "react";
 
 interface Props {
   algorithmsSelected: Algorithm[];
 }
 
+const initialIsSorting: SortingAlgorithms = {};
+
+Object.values(algorithms).forEach((algorithm) => {
+  if (!initialIsSorting[algorithm]) initialIsSorting[algorithm] = {};
+  Object.values(sortOptions).forEach((sortOption: SortOption) => {
+    initialIsSorting[algorithm][sortOption] = false;
+  });
+});
+
 export const Playground: React.FC<Props> = ({ algorithmsSelected }) => {
+  const [isSorting, setIsSorting] = useState(initialIsSorting);
+
+  const handleActivateColumn = (column: number) => () => {
+    setIsSorting((prevState) => {
+      const newState = structuredClone(prevState);
+      algorithmsSelected.forEach((algorithm) => {
+        Object.values(sortOptions).forEach(
+          (sortOption: SortOption, idx: number) => {
+            if (idx === column)
+              newState[algorithm][sortOption] =
+                !newState[algorithm][sortOption];
+          }
+        );
+      });
+
+      return newState;
+    });
+  };
+
+  const isSomeoneSorting = (sortOption: SortOption) => {
+    return algorithmsSelected.some(
+      (algorithm) => isSorting[algorithm][sortOption]
+    );
+  };
+
   return (
     <section className="playground-container">
       <div className="playground-type">
         <span></span>
-        <h2>Random</h2>
-        <h2>Reversed</h2>
-        <h2>Nearly sorted</h2>
+        <button
+          disabled={isSomeoneSorting(sortOptions.RANDOM)}
+          onClick={handleActivateColumn(0)}
+        >
+          Random
+        </button>
+        <button
+          disabled={isSomeoneSorting(sortOptions.REVERSED)}
+          onClick={handleActivateColumn(1)}
+        >
+          Reversed
+        </button>
+        <button
+          disabled={isSomeoneSorting(sortOptions.NEARLY_SORTED)}
+          onClick={handleActivateColumn(2)}
+        >
+          Nearly sorted
+        </button>
         {algorithmsSelected.map((algorithm) => {
           return (
             <>
               <p>{algorithm}</p>
               {Object.values(sortOptions).map((sortOption) => {
                 return (
-                  <PlaygroundBlock algorithm={algorithm} option={sortOption} />
+                  <PlaygroundBlock
+                    isSorting={isSorting[algorithm][sortOption]}
+                    algorithm={algorithm}
+                    option={sortOption}
+                    setIsSorting={setIsSorting}
+                  />
                 );
               })}
             </>
