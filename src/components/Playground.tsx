4819dcb605b5cbf3ad57925ce7f70d47824b98f1
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-10 21:27:34 -0600

    Remove global state of isSorting and simlate click when user clicks on a column

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,86 +1,51 @@
 import "./styles/Playground.css";
-import { Algorithm, SortOption, SortingAlgorithms } from "../types";
-import { algorithms, sortOptions } from "../consts";
+import { Algorithm } from "../types";
+import { sortOptions } from "../consts";
 import { PlaygroundBlock } from "./PlaygroundBlock";
-import { useState } from "react";
+import React from "react";
 
 interface Props {
   algorithmsSelected: Algorithm[];
 }
 
-const initialIsSorting: SortingAlgorithms = {};
-
-Object.values(algorithms).forEach((algorithm) => {
-  if (!initialIsSorting[algorithm]) initialIsSorting[algorithm] = {};
-  Object.values(sortOptions).forEach((sortOption: SortOption) => {
-    initialIsSorting[algorithm][sortOption] = false;
-  });
-});
-
 export const Playground: React.FC<Props> = ({ algorithmsSelected }) => {
-  const [isSorting, setIsSorting] = useState(initialIsSorting);
+  const handleColumnClick = (column: number) => {
+    algorithmsSelected.forEach((_, idx) => {
+      const block = document.querySelector(
+        `.playground-block-${idx}-${column}`
+      );
 
-  const handleActivateColumn = (column: number) => () => {
-    setIsSorting((prevState) => {
-      const newState = structuredClone(prevState);
-      algorithmsSelected.forEach((algorithm) => {
-        Object.values(sortOptions).forEach(
-          (sortOption: SortOption, idx: number) => {
-            if (idx === column)
-              newState[algorithm][sortOption] =
-                !newState[algorithm][sortOption];
-          }
-        );
-      });
-
-      return newState;
+      if (block instanceof HTMLElement) {
+        block.click();
+      }
     });
-  };
-
-  const isSomeoneSorting = (sortOption: SortOption) => {
-    return algorithmsSelected.some(
-      (algorithm) => isSorting[algorithm][sortOption]
-    );
   };
 
   return (
     <section className="playground-container">
       <div className="playground-type">
         <span></span>
-        <button
-          disabled={isSomeoneSorting(sortOptions.RANDOM)}
-          onClick={handleActivateColumn(0)}
-        >
-          Random
-        </button>
-        <button
-          disabled={isSomeoneSorting(sortOptions.REVERSED)}
-          onClick={handleActivateColumn(1)}
-        >
-          Reversed
-        </button>
-        <button
-          disabled={isSomeoneSorting(sortOptions.NEARLY_SORTED)}
-          onClick={handleActivateColumn(2)}
-        >
-          Nearly sorted
-        </button>
-        {algorithmsSelected.map((algorithm) => {
+        {Object.values(sortOptions).map((option, idx) => {
           return (
-            <>
+            <button onClick={() => handleColumnClick(idx)}>{option}</button>
+          );
+        })}
+
+        {algorithmsSelected.map((algorithm, x) => {
+          return (
+            <React.Fragment key={crypto.randomUUID()}>
               <p>{algorithm}</p>
-              {Object.values(sortOptions).map((sortOption) => {
+              {Object.values(sortOptions).map((sortOption, y) => {
                 return (
                   <PlaygroundBlock
-                    isSorting={isSorting[algorithm][sortOption]}
                     algorithm={algorithm}
                     option={sortOption}
-                    setIsSorting={setIsSorting}
+                    position={`${x}-${y}`}
                     key={`${algorithm}-${sortOption}`}
                   />
                 );
               })}
-            </>
+            </React.Fragment>
           );
         })}
       </div>
