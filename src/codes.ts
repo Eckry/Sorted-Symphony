9c0c7da1f83c6d2b4b8298ea009cc0e30369f30d
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-25 17:33:43 -0600

    add mergesort code for javascript, cpp and python

--- src/codes.ts
+++ src/codes.ts
@@ -90,7 +90,7 @@ for(int i = 0; i < n; i++){
   if (i < r) quickSort(arr, i, r);
   if (l < j) quickSort(arr, l, j);
 }`,
-python: `def quickSort(arr, l, r):
+    python: `def quickSort(arr, l, r):
   center = (l + r) // 2
   i = l
   j = r
@@ -136,7 +136,149 @@ python: `def quickSort(arr, l, r):
   if (l < j) quickSort(arr, l, j);
 }`,
   },
-  MergeSort: {},
+  MergeSort: {
+    javascript: `function merge(arr, p, q, r) {
+  const n1 = q - p + 1;
+  const n2 = r - q;
+  const L = new Array(n1);
+  const M = new Array(n2);
+    
+  for (let i = 0; i < n1; i++) L[i] = arr[p + i];
+  for (let j = 0; j < n2; j++) M[j] = arr[q + 1 + j];
+    
+  let i = 0;
+  let j = 0;
+  let k = p;
+    
+  while (i < n1 && j < n2) {
+    if (L[i] <= M[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = M[j];
+      j++;
+    }
+    k++;
+  }
+    
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+    
+  while (j < n2) {
+    arr[k] = M[j];
+    j++;
+    k++;
+  }
+}
+    
+function mergeSort(arr, l, r) {
+  if (l < r) {
+    const m = l + Math.floor((r - l) / 2);
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
+  }
+}`,
+    cpp: `void merge(int arr[], int p, int q, int r)
+{
+  int n1 = q - p + 1;
+  int n2 = r - q;
+  int L[n1];
+  int M[n2];
+    
+  for (int i = 0; i < n1; i++) L[i] = arr[p + i];
+  for (int j = 0; j < n2; j++) M[j] = arr[q + 1 + j];
+    
+  int i = 0;
+  int j = 0;
+  int k = p;
+    
+  while (i < n1 && j < n2)
+  {
+    if (L[i] <= M[j])
+    {
+      arr[k] = L[i];
+      i++;
+    }
+    else
+    {
+      arr[k] = M[j];
+      j++;
+    }
+    k++;
+  }
+    
+  while (i < n1)
+  {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+    
+  while (j < n2)
+  {
+    arr[k] = M[j];
+    j++;
+    k++;
+  }
+}    
+
+void mergeSort(int arr[], int l, int r)
+{
+  if (l < r)
+  {
+    int m = l + (r - l) / 2;
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
+  }
+}`,
+    python: `def merge(arr, p, q, r):
+  n1 = q - p + 1
+  n2 = r - q
+  L = [0] * n1
+  M = [0] * n2
+
+  for i in range(n1):
+    L[i] = arr[p + i]
+  for j in range(n2):
+    M[j] = arr[q + 1 + j]
+
+  i = 0
+  j = 0
+  k = p
+
+  while i < n1 and j < n2:
+    if L[i] <= M[j]:
+      arr[k] = L[i]
+      i += 1
+    else:
+      arr[k] = M[j]
+      j += 1
+    k += 1
+
+  while i < n1:
+    arr[k] = L[i]
+    i += 1
+    k += 1
+
+  while j < n2:
+    arr[k] = M[j]
+    j += 1
+    k += 1
+
+
+def mergeSort(arr, l, r):
+  if l < r:
+    m = l + (r - l) // 2
+    mergeSort(arr, l, m)
+    mergeSort(arr, m + 1, r)
+    merge(arr, l, m, r)
+`,
+  },
   InsertionSort: {},
   HeapSort: {},
 };
