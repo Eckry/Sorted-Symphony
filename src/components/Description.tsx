bdf319b035c6e0a16b2375c0bddbd63509a007ab
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-26 17:02:02 -0600

    add render for information

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,9 +1,18 @@
-import "./styles/Description.css"
+import "./styles/Description.css";
 import { useSelected } from "../hooks/useSelected";
+import { information } from "../information";
 
 export const Description = () => {
   const { selected } = useSelected();
+  
+  const description = information[selected].description;
 
+  const timeComplexity = {
+    Average: information[selected].time.avg,
+    "Best case": information[selected].time.best,
+    "Worst case": information[selected].time.worst,
+    "Space complexity": information[selected].time.space,
+  };
   return (
     <section>
       <header>
@@ -11,9 +20,18 @@ export const Description = () => {
       </header>
       <div className="description-container">
         <div>
-          <p>Description...</p>
+          <p>{description}</p>
         </div>
-        <aside className="extra-information">Time complexity</aside>
+        <aside className="extra-information">
+          <h2>Time complexity</h2>
+          {Object.entries(timeComplexity).map(([title, complexity]) => {
+            return (
+              <p key={title}>
+                {title}: {complexity}
+              </p>
+            );
+          })}
+        </aside>
       </div>
     </section>
   );
