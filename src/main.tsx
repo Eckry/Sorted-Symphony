ab0a9a5cc2c93c3983799f10da60b6ab39125880
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-26 18:58:59 -0600

    add two new fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { SelectedProvider } from "./context/Selected.tsx";
+import '@fontsource/kaushan-script';
+import '@fontsource/averia-serif-libre';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
