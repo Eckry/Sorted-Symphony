795e89e717d41f1b855ef1348962195c54906817
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-26 18:59:10 -0600

    change welcome phrase

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="header">
       <h1 className="header-title">Sorted Symphony</h1>
-      <p className="header-phrase">Hear whatever sort algorithm you want.</p>
+      <p className="header-phrase">Welcome to Sorted Symphony, where data sorting becomes a musical experience. Explore the harmony of organization as you witness algorithms dance to the tune of efficiency.</p>
     </header>
   );
 };
