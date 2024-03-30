ed2a52d66984c5a0bdb94b86325843bfc9a1b1bd
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-30 17:11:36 -0600

    change clipboard icon to one with text in it

--- src/icons.tsx
+++ src/icons.tsx
@@ -131,3 +131,36 @@ export const JavaIcon = () => (
     />
   </svg>
 );
+
+export const ClipboardIcon = () => (
+  <svg
+    width="62"
+    height="62"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    fill="none"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+    <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
+    <path d="M9 12h6" />
+    <path d="M9 16h6" />
+  </svg>
+);
+
+export const ClipboardCheckedIcon = () => (
+  <svg
+    width="62"
+    height="62"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    fill="none"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+    <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
+    <path d="M9 14l2 2l4 -4" />
+  </svg>
+);
