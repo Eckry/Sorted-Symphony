9a2b94527faa11a4602bd6752b6bbe5da20889c0
Eckry <112786854+Eckry@users.noreply.github.com>
2024-03-30 17:41:09 -0600

    import victor mono 500

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { SelectedProvider } from "./context/Selected.tsx";
-import '@fontsource/kaushan-script';
-import '@fontsource/averia-serif-libre';
+import "@fontsource/kaushan-script";
+import "@fontsource/averia-serif-libre";
+import '@fontsource/victor-mono/500.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
