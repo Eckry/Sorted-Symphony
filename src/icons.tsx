96e061cfd001cf5f66ea291970bd555a99f96500
Eckry <112786854+Eckry@users.noreply.github.com>
2024-04-01 16:19:24 -0600

    fix html properties in stopicon

--- src/icons.tsx
+++ src/icons.tsx
@@ -21,21 +21,19 @@ export const StopIcon = () => (
     width="32"
     height="32"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    strokeWidth="1.5"
     stroke="#2c3e50"
     fill="none"
-    stroke-linecap="round"
-    stroke-linejoin="round"
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path
       d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
-      stroke-width="0"
+      strokeWidth="0"
       fill="currentColor"
     />
     <path
       d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
-      stroke-width="0"
+      strokeWidth="0"
       fill="currentColor"
     />
   </svg>
