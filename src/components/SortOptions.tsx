8cd13b56dad5c8e5ef116bc54e7c5655b8695b55
Eckry <112786854+Eckry@users.noreply.github.com>
2024-04-08 21:39:49 -0600

    remove React.memo

--- src/components/SortOptions.tsx
+++ src/components/SortOptions.tsx
@@ -7,44 +7,45 @@ interface Props {
   isSorting: boolean;
 }
 
-export const SortOptions: React.FC<Props> = React.memo(
-  ({ shuffleElements, isSorting }) => {
-    const handleRandom = () => {
-      shuffleElements(sortOptions.RANDOM);
-    };
-    console.log("s")
-    const handleReversed = () => {
-      shuffleElements(sortOptions.REVERSED);
-    };
+export const SortOptions: React.FC<Props> = ({
+  shuffleElements,
+  isSorting,
+}) => {
+  const handleRandom = () => {
+    shuffleElements(sortOptions.RANDOM);
+  };
 
-    const handleNearlySorted = () => {
-      shuffleElements(sortOptions.NEARLY_SORTED);
-    };
+  const handleReversed = () => {
+    shuffleElements(sortOptions.REVERSED);
+  };
 
-    return (
-      <footer className="sortoptions-container">
-        <button
-          disabled={isSorting}
-          className="sortoption"
-          onClick={handleRandom}
-        >
-          {sortOptions.RANDOM}
-        </button>
-        <button
-          disabled={isSorting}
-          className="sortoption"
-          onClick={handleReversed}
-        >
-          {sortOptions.REVERSED}
-        </button>
-        <button
-          disabled={isSorting}
-          className="sortoption"
-          onClick={handleNearlySorted}
-        >
-          {sortOptions.NEARLY_SORTED}
-        </button>
-      </footer>
-    );
-  }
-);
+  const handleNearlySorted = () => {
+    shuffleElements(sortOptions.NEARLY_SORTED);
+  };
+
+  return (
+    <footer className="sortoptions-container">
+      <button
+        disabled={isSorting}
+        className="sortoption"
+        onClick={handleRandom}
+      >
+        {sortOptions.RANDOM}
+      </button>
+      <button
+        disabled={isSorting}
+        className="sortoption"
+        onClick={handleReversed}
+      >
+        {sortOptions.REVERSED}
+      </button>
+      <button
+        disabled={isSorting}
+        className="sortoption"
+        onClick={handleNearlySorted}
+      >
+        {sortOptions.NEARLY_SORTED}
+      </button>
+    </footer>
+  );
+};
