83d7045bb26301b60f3e03a45b2528ec80a23d0c
Eckry <112786854+Eckry@users.noreply.github.com>
2024-04-15 21:14:00 -0600

    add About page

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,3 +1,36 @@
+import "./styles/About.css";
+import { algorithms } from "../consts";
+import { Link } from "react-router-dom";
+import { RightArrowIcon } from "../icons";
+
 export const About = () => {
-  return <h1>hello</h1>;
+  return (
+    <main className="about-container">
+      <h1 className="about-title">About</h1>
+      <p className="about-text">
+        This was done with the purpose of helping those students or curious
+        people who decide to study sorting algorithms. I hope this page helps
+        you resolve your doubts about any algorithm. If you want to contribute
+        an algorithm or make any changes, you can make a pull request in the{" "}
+        <a
+          className="about-link"
+          target="_blank"
+          href="https://github.com/Eckry/Sorted-Symphony"
+        >
+          project's GitHub repository.
+        </a>
+      </p>
+      <p className="about-text">
+        Here is a list of the sorting algorithms so far:
+      </p>
+      <ul className="about-ul">
+        {Object.values(algorithms).map((algorithm) => {
+          return <li className="about-algorithm">{algorithm}</li>;
+        })}
+      </ul>
+      <Link to="/" className="link-orchestra">
+        Go to homepage <RightArrowIcon />
+      </Link>
+    </main>
+  );
 };
