f8604560bdb1528fb228a2f3c0b781478b1a8e7f
Eckry <112786854+Eckry@users.noreply.github.com>
2024-04-17 17:19:37 -0600

    add styles to contact page

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,3 +1,29 @@
+import { Link } from "react-router-dom";
+import "./styles/Contact.css"
+import { RightArrowIcon, SendIcon } from "../icons";
+
 export const Contact = () => {
-  return <h1>hello</h1>;
+  const handleOnSubmit = () => {};
+
+  return (
+    <main className="contact-container">
+      <h1 className="contact-title">Contact</h1>
+      <form className="contact-form" onSubmit={handleOnSubmit}>
+        <label className="contact-label" htmlFor="input-name">
+          Name
+          <input className="contact-input" id="input-name" type="text" />
+        </label>
+        <label className="contact-label" htmlFor="input-email">
+          Email
+          <input className="contact-input" id="input-email" type="email" />
+        </label>
+        <label className="contact-label" htmlFor="input-message">
+          Message
+          <textarea className="contact-input" id="input-message" cols={30} rows={10}></textarea>
+        </label>
+        <button className="contact-button">Send <SendIcon /></button>
+      </form>
+      <Link to="/" className="link-orchestra">Go to homepage <RightArrowIcon /></Link>
+    </main>
+  );
 };
