cb967b32fc631284085cb22473c8ae269c662011
Eckry <112786854+Eckry@users.noreply.github.com>
2024-04-17 18:08:04 -0600

    create Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,14 @@
+import "./styles/Loading.css"
+
+export const Loading = () => {
+  return (
+    <div className="typing-indicator">
+      <div className="typing-circle"></div>
+      <div className="typing-circle"></div>
+      <div className="typing-circle"></div>
+      <div className="typing-shadow"></div>
+      <div className="typing-shadow"></div>
+      <div className="typing-shadow"></div>
+    </div>
+  );
+};
