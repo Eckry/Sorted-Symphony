a17dac372b65111e9506abb267a3d3714bda9658
Eckry <112786854+Eckry@users.noreply.github.com>
2024-04-17 18:08:19 -0600

    add funcionality of Contact page

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,29 +1,91 @@
 import { Link } from "react-router-dom";
-import "./styles/Contact.css"
-import { RightArrowIcon, SendIcon } from "../icons";
+import "./styles/Contact.css";
+import { CheckIcon, ErrorIcon, RightArrowIcon, SendIcon } from "../icons";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { Loading } from "../components/Loading";
+
+const templateId = import.meta.env.VITE_TEMPLATE_ID;
+const serviceId = import.meta.env.VITE_SERVICE_ID;
+const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+
+const states = {
+  NULL: <SendIcon />,
+  LOADING: <Loading />,
+  SUCCESS: <CheckIcon />,
+  ERROR: <ErrorIcon />,
+};
+
+type loadingStates = keyof typeof states;
 
 export const Contact = () => {
-  const handleOnSubmit = () => {};
+  const [isLoading, setIsLoading] = useState<loadingStates>(() => {
+    const wasSend = localStorage.getItem("SENT");
+    if (wasSend) return "SUCCESS";
+    return "NULL";
+  });
+  const form = useRef<HTMLFormElement>(null);
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isLoading === "SUCCESS") return;
+    setIsLoading("LOADING");
+    if (form.current) {
+      emailjs
+        .sendForm(serviceId, templateId, form.current, {
+          publicKey: publicKey,
+        })
+        .then(() => {
+          setIsLoading("SUCCESS");
+          localStorage.setItem("SENT", "true");
+        })
+        .catch((e) => {
+          setIsLoading("ERROR");
+          console.error(e);
+        });
+    }
+  };
 
   return (
     <main className="contact-container">
       <h1 className="contact-title">Contact</h1>
-      <form className="contact-form" onSubmit={handleOnSubmit}>
+      <form ref={form} className="contact-form" onSubmit={handleOnSubmit}>
         <label className="contact-label" htmlFor="input-name">
           Name
-          <input className="contact-input" id="input-name" type="text" />
+          <input
+            required
+            name="from-name"
+            className="contact-input"
+            id="input-name"
+            type="text"
+          />
         </label>
         <label className="contact-label" htmlFor="input-email">
           Email
-          <input className="contact-input" id="input-email" type="email" />
+          <input
+            required
+            name="reply_to"
+            className="contact-input"
+            id="input-email"
+            type="email"
+          />
         </label>
         <label className="contact-label" htmlFor="input-message">
           Message
-          <textarea className="contact-input" id="input-message" cols={30} rows={10}></textarea>
+          <textarea
+            required
+            name="message"
+            className="contact-input"
+            id="input-message"
+            cols={30}
+            rows={10}
+          ></textarea>
         </label>
-        <button className="contact-button">Send <SendIcon /></button>
+        <button className="contact-button">Send {states[isLoading]}</button>
       </form>
-      <Link to="/" className="link-orchestra">Go to homepage <RightArrowIcon /></Link>
+      <Link to="/" className="link-orchestra">
+        Go to homepage <RightArrowIcon />
+      </Link>
     </main>
   );
 };
