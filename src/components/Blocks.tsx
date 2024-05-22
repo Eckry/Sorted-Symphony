e066a9c22c0dd05f48170bd97e94640a25dbfee5
Eckry <112786854+Eckry@users.noreply.github.com>
2024-05-21 18:58:37 -0600

    Add optimization in Blocks component

--- src/components/Blocks.tsx
+++ src/components/Blocks.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Block } from "../types";
 import "./styles/Blocks.css";
 
@@ -5,27 +6,47 @@ interface Props {
   blocks: Block[];
 }
 
+interface BlockProps {
+  height: number;
+  width: number;
+  color: string;
+  spanHeight: number;
+}
+
+const LineBlock: React.FC<BlockProps> = React.memo(
+  ({ height, width, color, spanHeight }) => {
+    return (
+      <div
+        key={crypto.randomUUID()}
+        className="block"
+        style={{
+          height: `${height}%`,
+          backgroundColor: color,
+          borderTop: `${width}px solid var(--highlight-color)`,
+        }}
+      >
+        <span
+          className="block-top"
+          style={{ height: `${spanHeight}%`, width: `${width}px` }}
+        ></span>
+      </div>
+    );
+  }
+);
+
 export const Blocks: React.FC<Props> = ({ blocks }) => {
   const width = (-1 / 190) * blocks.length + 39 / 19;
-
   return (
     <div className="blocks-container">
       {blocks.map(({ val, color }) => {
         return (
-          <div
-            key={crypto.randomUUID()}
-            className="block"
-            style={{
-              height: `${(val * 100) / blocks.length}%`,
-              backgroundColor: color,
-              borderTop: `${width}px solid var(--highlight-color)`
-            }}
-          >
-            <span
-              className="block-top"
-              style={{ height: `${100 / val}%`, width: `${width}px` }}
-            ></span>
-          </div>
+          <LineBlock
+            key={val}
+            height={(val * 100) / blocks.length}
+            width={width}
+            color={color}
+            spanHeight={100 / val}
+          />
         );
       })}
     </div>
